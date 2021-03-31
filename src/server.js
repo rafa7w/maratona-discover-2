6890615294bc306a9c3adb932f8c5f05107dd9f9
Rafael William <rafael.fim@hotmail.com>
2021-03-30 23:20:37 -0300

    routes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const server = express();
+const routes = require("./routes");
+
 
 // enable static services
 server.use(express.static("public"));
 
-server.get("/", (req, res) => {
-        return res.sendFile(__dirname + "/views/index.html"); 
-});
-
+server.use(routes)
 
 server.listen(3000, () => console.log("Server running..."));
