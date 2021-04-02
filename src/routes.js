7cfbeb574414e0add8c0cbdd3e714efe42a3d380
Rafael William <rafael.fim@hotmail.com>
2021-04-02 00:16:18 -0300

    array jobs and routes job

--- src/routes.js
+++ src/routes.js
@@ -12,11 +12,40 @@ const profile = {
         "vacation-per-year": 4
 };
 
-routes.get("/", (req, res) => res.render(views + "index")); 
+const jobs = [
+        {
+                id: 1,
+                name: "Pizzaria Guloso",
+                "daily-hours": 2,
+                "total-hours": 60,
+                createdAt: Date.now()
+        },
+        {
+                id: 2,
+                name: "OneTwo Project",
+                "daily-hours": 3,
+                "total-hours": 47,
+                createdAt: Date.now()
+        }
+];
+
+routes.get("/", (req, res) => res.render(views + "index", { jobs })); 
 routes.get("/job", (req, res) => res.render(views + "job")); 
 routes.get("/job/edit", (req, res) => res.render(views + "job-edit")); 
 routes.get("/profile", (req, res) => res.render(views + "profile", { profile })); 
 
-routes.post("/job", (req, res) => console.log(req.body)); 
+routes.post("/job", (req, res) => {
+        // { name: "James", "daily-hours": "3.1", "total-hours": "3" }
+        const lastId = jobs[jobs.length - 1]?.id || 1;
+
+        jobs.push({
+                id: lastId + 1,
+                name: req.body.name,
+                "daily-hours": req.body["daily-hours"],
+                "total-hours": req.body["total-hours"],
+                createdAt: Date.now()
+        });
+        return res.redirect("/");
+}); 
 
 module.exports = routes; 
