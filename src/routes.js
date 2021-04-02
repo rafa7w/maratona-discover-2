0e28c203afd9f7acfd19dfef786a42b191352711
Rafael William <rafael.fim@hotmail.com>
2021-04-01 21:51:33 -0300

    more routes and correcting references

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-        return res.sendFile(__dirname + "/views/index.html"); 
-});
+const basePath = __dirname + "/views";
 
+routes.get("/", (req, res) => res.sendFile(basePath + "/index.html")); 
+routes.get("/job", (req, res) => res.sendFile(basePath + "/job.html")); 
+routes.get("/job/edit", (req, res) => res.sendFile(basePath + "/job-edit.html")); 
+routes.get("/profile", (req, res) => res.sendFile(basePath + "/profile.html")); 
 
-
-module.exports = routes;
+module.exports = routes; 
