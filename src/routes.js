493e96bba7b652b45518fcb1b6f78a66b8e5425b
Rafael William <rafael.fim@hotmail.com>
2021-04-02 22:34:29 -0300

    set the path of views by the server

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const routes = express.Router();
 
-const views = __dirname + "/views/";
-
 const Profile = {
         data: {
                 name: "Rafael",
@@ -16,7 +14,7 @@ const Profile = {
 
         controllers: {
                 index(req, res) {
-                        return res.render(views + "profile", { profile: Profile.data });
+                        return res.render("profile", { profile: Profile.data });
                 },
 
                 update(req, res) {
@@ -82,11 +80,11 @@ const Job = {
                                 };
                         });
                         
-                        return res.render(views + "index", { jobs: updatedJobs });
+                        return res.render("index", { jobs: updatedJobs });
                 },
 
                 create(req, res) {
-                        return res.render(views + "job");
+                        return res.render("job");
                 },
 
                 save(req, res) {
@@ -114,7 +112,7 @@ const Job = {
 
                         job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"]);
                         
-                        return res.render(views + "job-edit", { job });
+                        return res.render("job-edit", { job });
                 },
 
                 update(req, res) {
--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const server = express();
 const routes = require("./routes");
+const path = require("path");
 
 // usando template engine
 server.set("view engine", "ejs");
+
+// mudar a localização da pasta views
+server.set("views", path.join(__dirname, "views"));
 
 // enable static services
 server.use(express.static("public"));
