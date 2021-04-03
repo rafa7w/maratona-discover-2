a2f8d4e874e181bda3752936fb5bd0f3bcfb1455
Rafael William <rafael.fim@hotmail.com>
2021-04-03 09:27:45 -0300

    job controller, job model, utils (services)

--- src/routes.js
+++ src/routes.js
@@ -1,146 +1,16 @@
 const express = require("express");
 const routes = express.Router();
 const ProfileController = require("./controllers/ProfileController");
-const Profile = require("./model/Profile");
+const JobController = require("./controllers/JobController");
 
-const Job = {
-        data: [
-                {
-                        id: 1,
-                        name: "Pizzaria Guloso",
-                        "daily-hours": 2,
-                        "total-hours": 60,
-                        createdAt: Date.now()
-                },
-                {
-                        id: 2,
-                        name: "OneTwo Project",
-                        "daily-hours": 3,
-                        "total-hours": 47,
-                        createdAt: Date.now()
-                }
-        ],
-
-        controllers: {
-                index(req, res) {
-                        const updatedJobs = Job.data.map((job) => {
-                                // ajustes no job    
-                                const remaining = Job.services.remainingDays(job);
-                                status = remaining <= 0 ? "done" : "progress";
-                        
-                                return {
-                                        ...job,
-                                        remaining,
-                                        status,
-                                        budget: Job.services.calculateBudget(job, Profile.get()["value-hour"])
-                                };
-                        });
-                        
-                        return res.render("index", { jobs: updatedJobs });
-                },
-
-                create(req, res) {
-                        return res.render("job");
-                },
-
-                save(req, res) {
-                        // { name: "James", "daily-hours": "3.1", "total-hours": "3" }
-                        const lastId = Job.data[Job.data.length - 1]?.id || 0;
-
-                        Job.data.push({
-                                id: lastId + 1,
-                                name: req.body.name,
-                                "daily-hours": req.body["daily-hours"],
-                                "total-hours": req.body["total-hours"],
-                                createdAt: Date.now()
-                        });
-                        return res.redirect("/");
-                },
-
-                show(req, res) {
-
-                        const jobId = req.params.id;
-                        const job = Job.data.find(job => Number(job.id) === Number(jobId));
-                       
-                        if (!job) {
-                                return res.send("Job not found!");
-                        };
-
-                        job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"]);
-                        
-                        return res.render("job-edit", { job });
-                },
-
-                update(req, res) {
-
-                        const jobId = req.params.id;
-                        const job  = Job.data.find(job => Number(job.id) === Number(jobId));
-
-                        if (!job) {
-                                return res.render("Job not found!");
-                        };
-
-                        const updatedJob = {
-                                ...job,
-                                name: req.body.name,
-                                "total-hours": req.body["total-hours"],
-                                "daily-hours": req.body["daily-hours"]
-                        };
-
-                        Job.data = Job.data.map(job => {
-                                
-                                if (Number(job.id) === Number(jobId)) {
-                                        job = updatedJob;
-                                };
-
-                                return job
-                        });
-
-                        res.redirect("/job/" + jobId);
-                }, 
-
-                delete(req, res) {
-                        
-                        const jobId = req.params.id;
-                        
-                        Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId));
-
-                        return res.redirect("/");
-                }
-        },
-        
-        services: {
-                remainingDays(job) {
-                        // cálculo de tempo restante
-                        const remainingDays =   (job["total-hours"] / job["daily-hours"]).toFixed();
-                        
-                        const createdDate = new Date(job.createdAt);
-                        const dueDay = createdDate.getDate() + Number(remainingDays);
-                        const dueDateInMs = createdDate.setDate(dueDay);
-                        
-                        const timeDiffInMs = dueDateInMs - Date.now();
-                        // transformar ms em dias
-                        const dayInMs = 1000 * 60 * 60 * 24;
-                        const dayDiff = Math.floor(timeDiffInMs / dayInMs);
-                        
-                        // restam tantos dias...
-                        return dayDiff;
-                },
-
-                calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
-        }
-};
-
-
-routes.get("/", Job.controllers.index); 
-routes.get("/job", Job.controllers.create); 
-routes.get("/job/:id", Job.controllers.show); 
+routes.get("/", JobController.index); 
+routes.get("/job", JobController.create); 
+routes.get("/job/:id", JobController.show); 
 routes.get("/profile", ProfileController.index); 
 
-routes.post("/job", Job.controllers.save); 
-routes.post("/job/:id", Job.controllers.update); 
-routes.post("/job/delete/:id", Job.controllers.delete); 
+routes.post("/job", JobController.save); 
+routes.post("/job/:id", JobController.update); 
+routes.post("/job/delete/:id", JobController.delete); 
 routes.post("/profile", ProfileController.update); 
-
 
 module.exports = routes; 
