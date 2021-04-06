6409d5198e8e20a9ce5c5ec817a801312290a0e6
Rafael William <rafael.fim@hotmail.com>
2021-04-06 20:01:20 -0300

    refactoring the save method in  JobController

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -12,13 +12,14 @@ module.exports =  {
                 const jobs = Job.get();
                 const lastId = jobs[jobs.length - 1]?.id || 0;
 
-                jobs.push({
+                Job.create({
                         id: lastId + 1,
                         name: req.body.name,
                         "daily-hours": req.body["daily-hours"],
                         "total-hours": req.body["total-hours"],
                         createdAt: Date.now()
                 });
+                
                 return res.redirect("/");
         },
 
